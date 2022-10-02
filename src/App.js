ad54374090e7b9ab6f27f8e02da65c8314cbf929
shimonshachar <simboy2000@gmail.com>
2022-10-02 11:21:30 +0300

    stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState ,useEffect} from 'react';
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput'
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    
+  }, []);
   const [showParagraph, setShowParagraph] = useState(false);  
   console.log("APP RUNNING");
   const toggleParagraphHandler = () => {
